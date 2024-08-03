97a39417e0a6289a229671adbcb86b700bd18b59
Mohamed Atef <proofesser@gmail.com>
2024-08-03 06:33:59 -0700

    Added Menu's specific functions on OverFlowMenuActivity

--- app/src/main/java/mo/ed/ismailia/yat/kotlin/feature/activity/overflow/OverFlowMenuActivity.kt
+++ app/src/main/java/mo/ed/ismailia/yat/kotlin/feature/activity/overflow/OverFlowMenuActivity.kt
@@ -1,6 +1,8 @@
 package mo.ed.ismailia.yat.kotlin.feature.activity.overflow
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,36 @@ class OverFlowMenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_over_flow_menu2)
+    }
+
+    /*
+    onCreateOptionsMenu
+    OnOptionsItemSelected
+    OnPrepareOptionsMenu
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.search_menu_id -> openSearchView()
+            R.id.edit_menu_id -> openEditView()
+            R.id.remove_item_id -> openRemoveView()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun openRemoveView() {
+
+    }
+
+    private fun openEditView() {
+
+    }
+
+    private fun openSearchView() {
+
     }
 }
